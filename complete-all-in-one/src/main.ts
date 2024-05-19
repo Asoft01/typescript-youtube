dc499315d3c4913be65da634daae59590c188658
Adeleke Hammed <lekhad19@gmail.com>
2024-05-19 08:49:59 +0100

    type errors

--- complete-all-in-one/src/main.ts
+++ complete-all-in-one/src/main.ts
@@ -1,6 +1,10 @@
 let username= 'Dave'; 
 console.log(username); 
 
-let a =  12; 
-let b= '6';
-let c = 2;
+let a: number =  12; 
+let b: number= 6;
+let c: number = 2;
+
+console.log(a / b); 
+
+console.log(c * b);
