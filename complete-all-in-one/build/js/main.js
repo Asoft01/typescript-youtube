c8a72ac78182c24c519b4ba7bba2713888adb0cc
Adeleke Hammed <lekhad19@gmail.com>
2024-05-19 09:23:10 +0100

    regular expression

--- complete-all-in-one/build/js/main.js
+++ complete-all-in-one/build/js/main.js
@@ -1,8 +1,25 @@
 "use strict";
-let username = 'Dave';
-console.log(username);
-let a = 12;
-let b = 6;
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+// let username= 'Dave'; 
+// console.log(username); 
+// let a: number =  12; 
+// let b: number= 6;
+// let c: number = 2;
+// console.log(a / b); 
+// console.log(c * b);
+////////////////////////////////////////////////////////////////
+// let myName: string = 'Dave';
+// myName = 42;
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+// let album: any;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- complete-all-in-one/src/main.ts
+++ complete-all-in-one/src/main.ts
@@ -1,10 +1,33 @@
-let username= 'Dave'; 
-console.log(username); 
+// let username= 'Dave'; 
+// console.log(username); 
 
-let a: number =  12; 
-let b: number= 6;
-let c: number = 2;
+// let a: number =  12; 
+// let b: number= 6;
+// let c: number = 2;
 
-console.log(a / b); 
+// console.log(a / b); 
 
-console.log(c * b);
+// console.log(c * b);
+
+////////////////////////////////////////////////////////////////
+// let myName: string = 'Dave';
+// myName = 42;
+
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+// let album: any;
+let album: string | number; 
+
+myName= 'John'; 
+meaningOfLife = 42; 
+isLoading = true;
+
+const sum = (a: number, b: string) => {
+    return a + b;
+}
+
+let postId: string | number;
+let isActive: number | boolean; 
+
+let re: RegExp = /\w+/g 
