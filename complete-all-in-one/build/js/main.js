45758486661d023c782a69178ac9d1711df11527
Adeleke Hammed <lekhad19@gmail.com>
2024-05-20 10:14:36 +0100

    math function

--- complete-all-in-one/build/js/main.js
+++ complete-all-in-one/build/js/main.js
@@ -34,3 +34,128 @@ guitars[0] = 1984;
 let tests = [];
 let bands = [];
 bands.push('Van Halen');
+// console.log(bands);
+// ///////////////////////////// Tuple ////////////////////////////
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+mixed = myTuple;
+// myTuple = mixed; 
+///////////////////// Objects //////////////////////////////////////
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObject = {
+    prop1: 'Dave',
+    prop2: true
+};
+// exampleObject.prop2 = false;
+exampleObject.prop1 = 'John';
+///////////////////////////////////////////////////////////////////////////////
+// type Guitarist = {
+//     name: string, 
+//     // active: boolean, 
+//     active?: boolean, 
+//     albums: (string | number)[]
+// }
+// let evh: Guitarist = {
+//     name: 'Eddie', 
+//     active: false, 
+//     albums: [1984, 5150, 'OU812']
+// }
+// let jp: Guitarist = {
+//     name: 'Jimmy', 
+//     active: true, 
+//     albums: ['I', 'II', 'IV']
+// }
+// // evh = jp;
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     return `Hello ${guitarist.name}!`;
+// }
+// // console.log(greetGuitarist(jp));
+// console.log(greetGuitarist(evh));
+//////////////////////////////// Alternative way using interface ////////////////////////////////////////
+// interface Guitarist {
+//     // name: string,
+//     name?: string, 
+//     active: boolean, 
+//     albums: (string | number)[]
+// }
+// let evh: Guitarist = {
+//     name: 'Eddie', 
+//     active: false, 
+//     albums: [1984, 5150, 'OU812']
+// }
+// let jp: Guitarist = {
+//     name: 'Jimmy', 
+//     active: true, 
+//     albums: ['I', 'II', 'IV']
+// }
+// // evh = jp;
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     // return `Hello ${guitarist.name?.toUpperCase()}!`;
+//     if(guitarist.name){
+//         return `Hello ${guitarist.name.toUpperCase()}!`;
+//     }
+//     return 'Hello';
+// }
+// console.log(greetGuitarist(jp));
+// console.log(greetGuitarist(evh));
+////////////////////////////////////// Enums ///////////////////////////
+// enums "Unlike most typescript featues, enums are not a type-level addition to javascript but something added to the language and runtime"
+// enum Grade {
+//     U = 1, 
+//     D, 
+//     C, 
+//     B, 
+//     A
+// }
+// console.log(Grade.U);
+///////////////////////////// Functions ///////////////////////////////////////
+// Type Aliases 
+// type stringOrNumber = string | number; 
+// type stringOrNumberArray = (string | number)[]
+// type Guitarist = {
+//     name?: string, 
+//     active: boolean, 
+//     albums: stringOrNumberArray
+// }
+// type UserId = stringOrNumber; 
+// // Literal types 
+// let myName: 'Dave'
+// let userName: 'Dave' | 'John' | 'Anny'; 
+// userName = 'Anny'
+/////////////////////////////////// Function ////////////////////////////////////////////////
+const add = (a, b) => {
+    return a + b;
+};
+// a function that does not return anything which does not have a side effect
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello');
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+// interface mathFunction {
+//     (a: number, b: number): number
+// } 
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+// optional parameters 
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
--- complete-all-in-one/src/main.ts
+++ complete-all-in-one/src/main.ts
@@ -46,5 +46,163 @@ guitars[0] = 1984;
 let tests = [] 
 let bands: string[] = [];
 bands.push('Van Halen');
+// console.log(bands);
+
+// ///////////////////////////// Tuple ////////////////////////////
+let myTuple: [string, number, boolean] = ['Dave', 42, true];
+
+let mixed = ['John', 1, false];
+
+mixed = myTuple; 
+// myTuple = mixed; 
+
+///////////////////// Objects //////////////////////////////////////
+let myObj: object; 
+myObj = []; 
+console.log(typeof myObj);
+myObj = bands; 
+myObj = {}
+const exampleObject = {
+    prop1: 'Dave', 
+    prop2: true
+}
+
+// exampleObject.prop2 = false;
+exampleObject.prop1 = 'John';
+
+///////////////////////////////////////////////////////////////////////////////
+// type Guitarist = {
+//     name: string, 
+//     // active: boolean, 
+//     active?: boolean, 
+//     albums: (string | number)[]
+// }
+
+// let evh: Guitarist = {
+//     name: 'Eddie', 
+//     active: false, 
+//     albums: [1984, 5150, 'OU812']
+// }
+
+// let jp: Guitarist = {
+//     name: 'Jimmy', 
+//     active: true, 
+//     albums: ['I', 'II', 'IV']
+// }
+
+// // evh = jp;
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     return `Hello ${guitarist.name}!`;
+// }
+
+// // console.log(greetGuitarist(jp));
+// console.log(greetGuitarist(evh));
+
+//////////////////////////////// Alternative way using interface ////////////////////////////////////////
+
+// interface Guitarist {
+//     // name: string,
+//     name?: string, 
+//     active: boolean, 
+//     albums: (string | number)[]
+// }
 
 
+// let evh: Guitarist = {
+//     name: 'Eddie', 
+//     active: false, 
+//     albums: [1984, 5150, 'OU812']
+// }
+
+// let jp: Guitarist = {
+//     name: 'Jimmy', 
+//     active: true, 
+//     albums: ['I', 'II', 'IV']
+// }
+
+// // evh = jp;
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     // return `Hello ${guitarist.name?.toUpperCase()}!`;
+//     if(guitarist.name){
+//         return `Hello ${guitarist.name.toUpperCase()}!`;
+//     }
+//     return 'Hello';
+// }
+
+// console.log(greetGuitarist(jp));
+// console.log(greetGuitarist(evh));
+////////////////////////////////////// Enums ///////////////////////////
+// enums "Unlike most typescript featues, enums are not a type-level addition to javascript but something added to the language and runtime"
+// enum Grade {
+//     U = 1, 
+//     D, 
+//     C, 
+//     B, 
+//     A
+// }
+// console.log(Grade.U);
+
+///////////////////////////// Functions ///////////////////////////////////////
+// Type Aliases 
+// type stringOrNumber = string | number; 
+
+// type stringOrNumberArray = (string | number)[]
+
+// type Guitarist = {
+//     name?: string, 
+//     active: boolean, 
+//     albums: stringOrNumberArray
+// }
+
+// type UserId = stringOrNumber; 
+
+// // Literal types 
+// let myName: 'Dave'
+
+// let userName: 'Dave' | 'John' | 'Anny'; 
+// userName = 'Anny'
+
+/////////////////////////////////// Function ////////////////////////////////////////////////
+const add = (a:number, b: number): number => {
+    return a + b;
+}
+
+// a function that does not return anything which does not have a side effect
+const logMsg= (message: any): void => {
+    console.log(message)
+}
+
+logMsg('Hello');
+logMsg(add(2, 3)); 
+
+let subtract = function(c: number, d: number): number {
+    return c - d; 
+}
+
+type mathFunction = (a: number, b: number) => number;
+
+// interface mathFunction {
+//     (a: number, b: number): number
+// } 
+
+let multiply: mathFunction = function (c, d) {
+    return c * d;
+}
+
+logMsg(multiply(2, 2));
+
+// optional parameters 
+const addAll = (a: number, b: number, c?: number): number => {
+    if(typeof c !== 'undefined'){
+        return a + b + c;
+    }
+    return a + b; 
+}
+
+const sumAll = (a: number = 10, b: number, c: number = 2): number => {
+    return a + b + c;
+}
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3)); 
+logMsg(sumAll(2, 3))
